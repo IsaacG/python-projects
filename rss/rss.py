ec32e22f7ec6172b274f7a1cae891b968ee2917b
Isaac G <zgithub@isaacgood.com>
2013-04-04 03:45:34 -0400

    Some thoughts on RSS

---
+++ rss/rss.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+# I want an RSS client
+# It should read URLs from a file
+# It should cache what it already fetched so it knows what is new
+# Should it poll?
+# I am using this to fetch graphical RSS items so it needs to embed the content in an HTML file
+# Maybe have it spit out one HTML file per fetch? Then I can loop through HTML files, open and delete
