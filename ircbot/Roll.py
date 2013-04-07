2041b5bdea6535b92021a854cee70ec6e235be99
Isaac G <zgithub@isaacgood.com>
2013-04-07 14:00:58 -0400

    ircbot module Roll: dropped the channel whitelisting

--- ircbot/Roll.py
+++ ircbot/Roll.py
@@ -5,18 +5,17 @@ import random
 random.seed()
 
 def hookCode ( server, data ):
-	if data['Channel'].lower() in [ '#some_channel', '#other_channel' ]:
-		parts = data['Message'].split()
-		if parts[0] == '.roll':
-			if len ( parts ) == 1:
-				server.msg ( data['Channel'], "{}: you rolled a {}".format ( data['User']['Nick'], str ( random.randint ( 1, 6 ) ) ) )
-			elif len ( parts ) == 2:
-				server.msg ( data['Channel'], "{}: you rolled a {}".format ( data['User']['Nick'], random.randint ( 1, int ( parts[1] ) ) ) )
-			elif len ( parts ) == 3:
-				sum = 0
-				for x in range ( int ( parts[1] ) ):
-					sum += random.randint ( 1, int ( parts[2] ) )
-				server.msg ( data['Channel'], "{}: you rolled a {}".format ( data['User']['Nick'], sum ) )
+	parts = data['Message'].split()
+	if parts[0] == '.roll':
+		if len ( parts ) == 1:
+			server.msg ( data['Channel'], "{}: you rolled a {}".format ( data['User']['Nick'], str ( random.randint ( 1, 6 ) ) ) )
+		elif len ( parts ) == 2:
+			server.msg ( data['Channel'], "{}: you rolled a {}".format ( data['User']['Nick'], random.randint ( 1, int ( parts[1] ) ) ) )
+		elif len ( parts ) == 3:
+			sum = 0
+			for x in range ( int ( parts[1] ) ):
+				sum += random.randint ( 1, int ( parts[2] ) )
+			server.msg ( data['Channel'], "{}: you rolled a {}".format ( data['User']['Nick'], sum ) )
 
 name = 'Roll'
 
