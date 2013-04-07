7ec010a31132482c79e5ef85782b936c0430faab
Isaac G <zgithub@isaacgood.com>
2013-04-07 12:43:19 -0400

    ircbot module Greet: dMsg() was renames to msg(). Fixing

--- ircbot/Greet.py
+++ ircbot/Greet.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 def hookCode ( server, data ):
-	server.dMsg ( data['Channel'], "Hello " + data['User']['Nick'] )
+	server.msg ( data['Channel'], "Hello " + data['User']['Nick'] )
 
 name = 'Greet'
 
