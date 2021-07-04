b0f56ac8fb3d9431c759fdcd2af3d9b97390077c
Isaac Good <nuc-git@isaacgood.com>
2021-07-04 15:45:07 -0700

    Fix IRC bot

--- sopel/chan_manage.py
+++ sopel/chan_manage.py
@@ -12,8 +12,8 @@ This is a custom channel management bot. Features include:
 """
 
 import functools
-import more_itertools
 import re
+import more_itertools
 import sopel
 
 
@@ -31,11 +31,13 @@ class NickManager:
 
   def __init__(self, bot):
     self.bot = bot
+    self.banned_re = None
+    self.vetted_re = None
     self.reload()
 
   def re_join(self, patterns):
     """Join RE's with an OR."""
-    patt = '|'.join('(%s)' % p for p in patterns)
+    patt = '|'.join('(%s)' % p.lower() for p in patterns)
     return re.compile(patt, re.IGNORECASE)
 
   def reload(self):
@@ -49,9 +51,9 @@ class NickManager:
 
     # Nicks should be treated as string literals so escape them.
     self.vetted_re = self.re_join(re.escape(v) for v in self.vetted)
-    # Ops and banned are hostmasks. Convert to hostmask RE.
+    self.ops_accounts = [i.lower() for i in self.ops.values()]
+    # Banned are hostmasks. Convert to hostmask RE.
     self.banned_re = self.re_join(sopel.tools.get_hostmask_regex(b).pattern for b in self.banned)
-    self.ops_re = self.re_join(sopel.tools.get_hostmask_regex(o).pattern for o in self.ops.values())
     # Ban words are regexes. Just join them.
     self.bad_words_re = self.re_join(self.bad_words)
     self.bad_names_re = self.re_join(self.bad_names)
@@ -60,9 +62,9 @@ class NickManager:
     """Return if the nick or user is offensive."""
     return self.bad_names_re.search(trigger.nick) or self.bad_names_re.search(trigger.user)
 
-  def is_op(self, hostmask):
+  def is_op(self, trigger):
     """Return if the hostmask matches the ops hostmasks."""
-    return bool(self.ops_re.match(hostmask))
+    return trigger.account and trigger.account.lower() in self.ops_accounts
 
   def is_banned(self, trigger):
     """Return if the user matches a hostmask on the ban list."""
@@ -74,7 +76,7 @@ class NickManager:
 
   def is_vetted(self, trigger):
     """Return if the user is on the vetted list."""
-    return self.is_vetted_user(trigger.nick)
+    return bool(self.is_vetted_user(trigger.nick) or trigger.admin or self.is_op(trigger))
 
   def add_vetted(self, nick):
     """Add a nick to the vetted list."""
@@ -83,7 +85,7 @@ class NickManager:
       return
 
     self.vetted.add(nick)
-    bot.db.set_plugin_value(PLUGIN, 'vetted', list(self.vetted))
+    self.bot.db.set_plugin_value(PLUGIN, 'vetted', list(self.vetted))
     self.vetted_re = self.re_join(re.escape(v) for v in self.vetted)
 
   def drop_vetted(self, nick):
@@ -93,7 +95,7 @@ class NickManager:
       return
 
     self.vetted.remove(nick)
-    bot.db.set_plugin_value(PLUGIN, 'vetted', list(self.vetted))
+    self.bot.db.set_plugin_value(PLUGIN, 'vetted', list(self.vetted))
     self.vetted_re = self.re_join(re.escape(v) for v in self.vetted)
 
   def add_ban(self, mask):
@@ -103,7 +105,7 @@ class NickManager:
       return
 
     self.banned.add(mask)
-    bot.db.set_plugin_value(PLUGIN, 'banned', list(self.banned))
+    self.bot.db.set_plugin_value(PLUGIN, 'banned', list(self.banned))
     self.banned_re = self.re_join(sopel.tools.get_hostmask_regex(b).pattern for b in self.banned)
 
   def drop_ban(self, mask):
@@ -112,8 +114,8 @@ class NickManager:
     if mask not in self.banned:
       return
 
-    self.banned.remove(b)
-    bot.db.set_plugin_value(PLUGIN, 'banned', list(self.banned))
+    self.banned.remove(mask)
+    self.bot.db.set_plugin_value(PLUGIN, 'banned', list(self.banned))
     self.banned_re = self.re_join(sopel.tools.get_hostmask_regex(b).pattern for b in self.banned)
 
   def bad_message(self, message):
@@ -122,7 +124,10 @@ class NickManager:
 
   def log(self, trigger, message):
     """Log to the associated log channel."""
-    destination = self.channels[trigger.sender.lower()]
+    if trigger.sender.lower() in self.channels:
+      destination = self.channels[trigger.sender.lower()]
+    else:
+      destination = list(self.channels.values())[0]
     self.bot.say(message, destination)
 
 
@@ -150,10 +155,12 @@ def kickban(bot, channel, nick, mask):
 
 @functools.cache
 def manager(bot):
+  """Return the cached manager."""
   return NickManager(bot)
-    
+
 
 def get_channels(bot, trigger):
+  """Return channels to act upon from the trigger details."""
   m_chans = manager(bot).channels
   parts = trigger.lower().split()
   if parts:
@@ -173,8 +180,9 @@ def require_vetted(is_vetted):
   def actual_decorator(function):
     @functools.wraps(function)
     def guarded(bot, trigger, *args, **kwargs):
-      if is_vetted == (trigger.admin or manager(bot).is_vetted(trigger)):
+      if is_vetted == manager(bot).is_vetted(trigger):
         return function(bot, trigger, *args, **kwargs)
+      return lambda *args, **kwargs: None
     return guarded
   return actual_decorator
 
@@ -184,8 +192,9 @@ def require_op():
   def actual_decorator(function):
     @functools.wraps(function)
     def guarded(bot, trigger, *args, **kwargs):
-      if manager(bot).is_op(trigger.hostmask):
+      if manager(bot).is_op(trigger):
         return function(bot, trigger, *args, **kwargs)
+      return lambda *args, **kwargs: None
     return guarded
   return actual_decorator
 
@@ -198,9 +207,9 @@ def require_word_count(minimum, maximum=None):
       parts = trigger.split()
       words = len(parts) - 1
       if words < minimum:
-        return
+        return lambda *args, **kwargs: None
       if maximum and words > maximum:
-        return
+        return lambda *args, **kwargs: None
       return function(bot, trigger, *args, **kwargs)
     return guarded
   return actual_decorator
@@ -221,7 +230,7 @@ def voice_users(bot, channel):
   privileges = chan.privileges
   give_voice = []
 
-  for nick, user in users.items():
+  for nick in users:
     if privileges[nick] & sopel.module.VOICE:
       continue
     if not manager(bot).is_vetted_user(nick):
@@ -285,35 +294,43 @@ def voiceall(bot, trigger):
 @require_vetted(False)
 def bad_message(bot, trigger):
   """Ban unvetted users for poor behavior."""
+  if trigger.sender not in manager(bot).channels:
+    return
+
   m = manager(bot)
   msg = None
 
   # Check for bad words.
-  if not msg and m.bad_message(trigger):
+  if msg is None and m.bad_message(trigger):
     msg = f'Banning {trigger.nick} for using bad words. Said: {trigger}'
 
   # Check for a repeat pattern.
-  if not msg and REPEAT_RE.search(trigger.lower()):
+  if msg is None and REPEAT_RE.search(trigger.lower()):
     msg = f'Banning {trigger.nick} for being repetitive.'
 
   # Check for excessive highlighting.
-  if not msg:
+  if msg is None:
     words = trigger.lower().split()
     users = [u.lower() for u in bot.channels[trigger.sender].users]
     highlight_count = sum(w in users for w in words)
-    if highlight_count > len(words) // 2 or highlight_count > 4:
+    if len(words) > 2 and (highlight_count > (len(words) // 2) or highlight_count > 4):
       msg = f'Banning {trigger.nick} for hilighting {highlight_count} times'
 
-  if msg:
-    m.log(trigger, msg)
+  if msg is not None:
+    print(f'bad_message ban on {trigger!r} with msg {msg!r}')
     for c in manager(bot).channels:
       kickban(bot, c, trigger.nick, f'*!*@{trigger.host}')
+    m.log(trigger, msg)
 
 
 @sopel.module.event('NOTICE')
 @require_vetted(False)
 def on_notice(bot, trigger):
   """Ban unvetted users for using NOTICE."""
+  if trigger.sender not in manager(bot).channels:
+    return
+
+  msg = f'Banning {trigger.nick} for using a NOTICE.'
   for c in manager(bot).channels:
     kickban(bot, c, trigger.nick, f'*!*@{trigger.host}')
   manager(bot).log(trigger, msg)
@@ -358,7 +375,7 @@ def kick(bot, trigger):
     bot.kick(nick, c)
 
 
-@sopel.module.rule(r'\+b ')
+@sopel.module.rule(r'\+b ', r'ban ')
 @sopel.module.require_privmsg()
 @require_word_count(1)
 @require_op()
@@ -368,6 +385,8 @@ def add_ban(bot, trigger):
   print(f'Banning {nick}')
   if '!' in nick and '@' in nick:
     mask = nick
+  elif '@' in nick and '!' not in nick:
+    mask = f'*!{nick}'
   elif '!' not in nick and '@' not in nick:
     mask = f'{nick}!*@*'
   else:
@@ -375,18 +394,18 @@ def add_ban(bot, trigger):
     return
   print(f'Banning m {mask}')
   manager(bot).add_ban(mask)
-  
+
   # Apply the mask to all users and kick anyone that matches.
-  re = sopel.tools.get_hostmask_regex(mask)
+  ban_re = sopel.tools.get_hostmask_regex(mask)
   for c in get_channels(bot, trigger):
     for user in bot.channels[c].users.values():
-      if re.match(user.hostmask):
+      if ban_re.match(user.hostmask):
         kickban(bot, c, user.nick, f'*!*@{user.host}')
-        msg = f'Kick ban {user.nick} from {c}. New ban: {mask}'
+        msg = f'Kick ban {user.nick} from {c} by {trigger.nick}. New ban: {mask}'
         manager(bot).log(trigger, msg)
 
 
-@sopel.module.rule(r'-b ')
+@sopel.module.rule(r'-b ', r'ban ')
 @sopel.module.require_privmsg()
 @require_word_count(1)
 @require_op()
@@ -426,7 +445,10 @@ def on_join(bot, trigger):
 
   m = manager(bot)
 
-  if m.is_banned(trigger):
+  if trigger.nick == bot.nick:
+    print('Connected. Try to op up.')
+    bot.say(f'op {trigger.sender}', 'chanserv')
+  elif m.is_banned(trigger):
     kickban(bot, trigger.sender, trigger.nick, f'*!*@{trigger.host}')
     msg = f'Kick ban {trigger.hostmask}. They are on the ban list.'
     m.log(trigger, msg)
@@ -441,8 +463,9 @@ def on_join(bot, trigger):
 
 @sopel.module.rule('help')
 @sopel.module.require_privmsg()
-def help(bot, trigger):
+def help_msg(bot, trigger):
   """List bot help."""
+  _ = trigger
   commands = [
     'ops                 - channel only; anyone; list/hilight ops',
     'help                - PM only; anyone; list this help',
@@ -465,19 +488,11 @@ def help(bot, trigger):
 def ops(bot, trigger):
   """List ops."""
   chan_users = bot.channels[trigger.sender].users.values()
-  ignore = {'not_yitz', 'chanslave', 'chanslave_v2'}
-  ops = [u.nick for u in chan_users if manager(bot).is_op(u.hostmask) and u.nick not in ignore]
+  ops_accounts = manager(bot).ops_accounts
+  op_list = [u.nick for u in chan_users if u.account and u.account.lower() in ops_accounts]
+  op_list = [i for i in op_list if i != bot.nick and not i.startswith('not')]
 
-  bot.say(f'Ops: {", ".join(sorted(ops))}')
-
-@sopel.module.event(sopel.tools.events.RPL_ENDOFNAMES)
-def on_conn(bot, trigger):
-  """Try to op-up on connecting to a channel."""
-  print('RPL_ENDOFNAMES:', trigger.sender, trigger)
-  if trigger.sender not in manager(bot).channels:
-    return
-  print('Connected. Try to op up.')
-  bot.say(f'op {trigger.sender}', 'chanserv')
+  bot.say(f'Ops: {", ".join(sorted(op_list))}')
 
 
 @sopel.module.rule(r'.*')
@@ -488,10 +503,21 @@ def forward_pms(bot, trigger):
   bot.say(f'{trigger.nick}: {trigger} ', destination=owner)
 
 
-@sopel.module.commands('a')
+@sopel.module.commands('mreload')
 @sopel.module.require_privmsg()
 @sopel.module.require_owner()
+def mreload(bot, trigger):
+  """Reload manager."""
+  manager(bot).reload()
+
+
+@sopel.module.commands('a')
+@sopel.module.require_privmsg()
+# @sopel.module.require_owner()
 def a(bot, trigger):
   """Test."""
-  print(trigger)
-  bot.say(trigger)
+  bot.say(repr(manager(bot).is_vetted(trigger)))
+  # bot.say(repr(manager(bot).bad_words))
+  chan_users = bot.channels['##bdsmcommunity'].users.values()
+  nu = [str(u.nick) for u in chan_users if u.account is None]
+  bot.say(" ".join(nu))
