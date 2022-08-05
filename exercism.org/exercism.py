6154cb00d8d7acb9812223cfedb827c1e040509f
Isaac Good <nuc-git@isaacgood.com>
2022-08-05 15:27:52 -0700

    Handle rate limiting and retries

--- exercism.org/exercism.py
+++ exercism.org/exercism.py
@@ -4,6 +4,7 @@
 import collections
 import datetime
 import json
+import logging
 import os
 import pathlib
 import tenacity
@@ -11,7 +12,6 @@ import time
 from typing import Any, Callable, Iterable
 
 # External libs
-import dateparser
 import requests
 
 
@@ -34,22 +34,28 @@ class Exercism:
         self.session = requests.Session()
         self.session.headers.update({"Authorization": f"Bearer {token}"})
 
+    def request(self, func, *args, **kwargs) -> requests.Response:
+        resp = func(*args, **kwargs)
+        if "retry-after" in resp.headers:
+            delay = int(resp.headers["retry-after"])
+            print(f"Rate limited. Sleep {delay} and retry.")
+            time.sleep(delay + 1)
+            resp = func(*args, **kwargs)
+
+        resp.raise_for_status()
+        time.sleep(kwargs["sleep"])
+
     @tenacity.retry(
         stop=tenacity.stop_after_attempt(3),
         wait=tenacity.wait_exponential(multiplier=2, min=15, max=60),
         retry=tenacity.retry_if_exception_type((requests.HTTPError, requests.exceptions.ConnectionError)),
     )
     def get_json_with_retries(self, *args, sleep=0.2, **kwargs) -> object:
-        resp = self.session.get(*args, **kwargs)
-        resp.raise_for_status()
-        time.sleep(sleep)
+        resp = self.request(self.session.get, *args, sleep=sleep, **kwargs)
         return resp.json()
 
     def post(self, *args, sleep=0.5, **kwargs) -> object:
-        resp = self.session.post(*args, **kwargs)
-        resp.raise_for_status()
-        time.sleep(sleep)
-        return resp
+        return self.request(self.session.post, *args, sleep=sleep, **kwargs)
 
     def notifications(self) -> Notifications:
         """Return notifications."""
@@ -94,7 +100,7 @@ class Exercism:
         """Return mentor discussion posts for one discussion."""
         return self.get_json_with_retries(f"{self.API}/mentoring/discussions/{uuid}/posts")["items"]
 
-    def old_mentor_discussions(self, status: str, order: str = "oldest", age: int):
+    def old_mentor_discussions(self, status: str, age: int, order: str = "oldest") -> list[str]:
         """Get mentor discussions more than a certain age (days)."""
         assert order in ("oldest", "recent", "exercise", "student")
         assert status in ("awaiting_mentor", "awaiting_student", "finished")
@@ -104,19 +110,20 @@ class Exercism:
         page_count = resp["meta"]["total_pages"]
 
         delta = datetime.timedelta(days=age)
-        cutoff = datetime.datetime.now(tz=datetime.timezone.utc) - delta
+        cutoff = datetime.datetime.now() - delta
         uuids = []
         for page in range(1, page_count + 1):
+            logging.info(f"Fetching old discussions, page {page} of {page_count}")
             params["page"] = page
             resp = self.get_json_with_retries(f"{self.API}/mentoring/discussions", params=params)
             for discussion in resp["results"]:
-                updated = dateparser.parse(discussion["updated_at"])
+                updated = datetime.datetime.strptime(discussion["updated_at"], "%Y-%m-%dT%H:%M:%SZ")
                 if updated > cutoff:
                     continue
 
                 uuid = discussion["uuid"]
                 posts = self.get_json_with_retries(f"{self.API}/mentoring/discussions/{uuid}/posts")["items"]
-                post_dates = [dateparser.parse(c['updated_at']) for c in posts]
+                post_dates = [datetime.datetime.strptime(c['updated_at'], "%Y-%m-%dT%H:%M:%SZ") for c in posts]
                 most_recent_post = max(post_dates)
                 if most_recent_post > cutoff:
                     continue
@@ -135,9 +142,40 @@ class Exercism:
                 "If you are still working on this, that is great, too! "
                 "If you have any questions or what any help or tips, just let me know!"
             )
+        print(f"Nudging {len(uuids)} discussions.")
         for uuid in uuids:
             self.post(f"{self.API}/mentoring/discussions/{uuid}/posts", data={"content": msg})
 
+    def failing_solutions(self, track: None | str = None):
+        """Get solutions which are not passing."""
+        params = {}
+        resp = self.get_json_with_retries(f"{self.API}/solutions", params=params)
+        page_count = resp["meta"]["total_pages"]
+
+        solutions = []
+        for page in range(1, page_count + 1):
+            logging.info(f"Fetching failing solutions, page {page} of {page_count}")
+            params["page"] = page
+            resp = self.get_json_with_retries(f"{self.API}/solutions", params=params)
+            for solution in resp["results"]:
+                if track and solution["track"]["slug"] != track:
+                    continue
+                if solution["published_iteration_head_tests_status"] in ("passed", "not_queued"):
+                    continue
+                solutions.append({
+                    f: solution[f] for f in ("uuid", "private_url", "published_iteration_head_tests_status", "published_at", "completed_at", "updated_at", "is_out_of_date")
+                })
+                for f in ("exercise", "track"):
+                    solutions[-1][f] = solution[f]["slug"]
+        return solutions
+
+
+def nudge():
+    e = Exercism()
+    ids = e.old_mentor_discussions("awaiting_student", 30)
+    e.nudge(ids)
 
 if __name__ == "__main__":
-    Exercism().print_unread_notifications()
+    # Exercism().print_unread_notifications()
+    for exercise in Exercism().failing_solutions("python"):
+        print(f"https://exercism.org/tracks/{exercise['track']}/exercises/{exercise['exercise']}")
